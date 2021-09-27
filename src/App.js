dcb81e99e9e14a63deb80601ec8a17ec0f651729
Iann-rst <iannrst11@gmail.com>
2021-09-27 12:40:04 -0300

    Utilizando Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+/*Hook para utilizar os metodos de ciclo de vida dos componentes:
+  useEffect:
+    *Usado para DidMount
+    *DidUptade
+    *WillUnMount
+*/
 function App() {
-  const [repositorio, setRepositorio] = useState([
-    { id:1, name: 'repo-1'},
-    { id:2, name: 'repo-2'},
-    { id:3, name: 'repo-3'},
-  ]);
+  /** useState()
+   * [state, setState] = useState(estado inicial);
+   */
+  const [repositorio, setRepositorio] = useState([]);
 
-  function handleAddRepository(){
-    setRepositorio([
-      ...repositorio,
-      {id: Math.random(), name: "Novo repo"}
-    ]);
+  //Equivale ao ComponentDidMount - executado apenas uma vez, na criação do componente
+  //Muito utilizado para carregar dados de uma api
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const response = await fetch('https://api.github.com/users/Iann-rst/repos');
+      const data = await response.json();
+      
+    setRepositorio(data);
+    }
+    fetchData();
+
+  }, []);
+
+  //Equivale ao ComponentDidUpdate - disparar toda vez que a variável 'repositorio' mudar
+  useEffect(()=>{
+    const filtered = repositorio.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositorio]);
+
+
+
+
+  function handleFavorite(id){
+    const novoRepositorio = repositorio.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    })
+    setRepositorio(novoRepositorio);
   }
+
   return (
     <> 
       <ul>
         {repositorio.map(repo => (
-          <li key = {repo.id}>{repo.name}</li>
+          <li key = {repo.id}>{repo.name}
+          {repo.favorite && <span> (Favorito) </span>}
+            <button onClick={() => handleFavorite(repo.id)}>Tornar Favorito</button>
+          </li>
         ))}
       </ul>
-      <button onClick={handleAddRepository}>Adicionar Repositório</button>
     </>
   );
 }
